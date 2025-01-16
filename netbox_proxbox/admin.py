66f3f8c9e36abe513f060363b9abe689ad5a3af8
Emerson Felipe <emerson@netdevopsbr.com>
2025-01-16 15:45:05 +0000

    Deletes stale 'admin' module from plugin

--- netbox_proxbox/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ProxmoxVM
-
-@admin.register(ProxmoxVM)
-class ProxmoxVMAdmin(admin.ModelAdmin):
-    list_display = ("cluster", "node", "virtual_machine", "proxmox_vm_id")
