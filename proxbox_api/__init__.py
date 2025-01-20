9db30f96c54fe9ee1e421d637843069000b5de63
Emerson Felipe <emerson@netdevopsbr.com>
2025-01-20 14:56:29 +0000

    Start changing imports from 'netbox_proxbox.backend' to 'proxbox_api'

--- proxbox_api/__init__.py
+++ proxbox_api/__init__.py
@@ -1,12 +1,12 @@
-from netbox_proxbox.backend.routes.netbox.virtualization.cluster_type import ClusterType
-from netbox_proxbox.backend.routes.netbox.virtualization.cluster import Cluster
-from netbox_proxbox.backend.routes.netbox.virtualization.virtual_machines import VirtualMachine
+from proxbox_api.routes.netbox.virtualization.cluster_type import ClusterType
+from proxbox_api.routes.netbox.virtualization.cluster import Cluster
+from proxbox_api.routes.netbox.virtualization.virtual_machines import VirtualMachine
 
-from netbox_proxbox.backend.routes.netbox.virtualization.interfaces import VMInterface
+from proxbox_api.routes.netbox.virtualization.interfaces import VMInterface
 
-from netbox_proxbox.backend.routes.netbox.dcim.sites import Site
-from netbox_proxbox.backend.routes.netbox.dcim.device_roles import DeviceRole
-from netbox_proxbox.backend.routes.netbox.dcim.device_types import DeviceType
-from netbox_proxbox.backend.routes.netbox.dcim.devices import Device
-from netbox_proxbox.backend.routes.netbox.dcim.interfaces import Interface
-from netbox_proxbox.backend.routes.netbox.ipam.ip_addresses import IPAddress
+from proxbox_api.routes.netbox.dcim.sites import Site
+from proxbox_api.routes.netbox.dcim.device_roles import DeviceRole
+from proxbox_api.routes.netbox.dcim.device_types import DeviceType
+from proxbox_api.routes.netbox.dcim.devices import Device
+from proxbox_api.routes.netbox.dcim.interfaces import Interface
+from proxbox_api.routes.netbox.ipam.ip_addresses import IPAddress
