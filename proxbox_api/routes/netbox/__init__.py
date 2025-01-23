1464ddf25884b759e73b6fd43842609854e841a8
Emerson Felipe <emerson@netdevopsbr.com>
2025-01-23 21:07:05 +0000

    Start using SQLModel on proxbox_api package to create NetBox endpoint on FastAPI (backend)

--- proxbox_api/routes/netbox/__init__.py
+++ proxbox_api/routes/netbox/__init__.py
@@ -1,16 +1,50 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, HTTPException
+from sqlmodel import select
 
 from typing import Annotated, Any
-
 from proxbox_api.routes.proxbox import netbox_settings
 from proxbox_api.session.netbox import NetboxSessionDep
-
+from proxbox_api.database import SessionDep, NetBoxEndpoint
 # FastAPI Router
 router = APIRouter()
 
 #
 # Endpoints: /netbox/<endpoint>
 #
+
+@router.post('/endpoint')
+def create_netbox_endpoint(netbox: NetBoxEndpoint, session: SessionDep) -> NetBoxEndpoint:
+    session.add(netbox)
+    session.commit()
+    session.refresh(netbox)
+    return netbox
+
+@router.get('/endpoint')
+def get_netbox_endpoints(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100
+) -> list[NetBoxEndpoint]:
+    netbox_endpoints = session.exec(select(NetBoxEndpoint).offset(offset).limit(limit)).all()
+    return netbox_endpoints
+
+@router.get('/endpoint/{netbox_id}')
+def get_netbox_endpoint(netbox_id: int, session: SessionDep) -> NetBoxEndpoint:
+    netbox_endpoint = session.get(NetBoxEndpoint, netbox_id)
+    if not netbox_endpoint:
+        raise HTTPException(status_code=404, detail="Netbox Endpoint not found")
+    return netbox_endpoint
+
+@router.delete('/endpoint/{netbox_id}')
+def delete_netbox_endpoint(netbox_id: int, session: SessionDep) -> dict:
+    netbox_endpoint = session.get(NetBoxEndpoint, netbox_id)
+    if not netbox_endpoint:
+        raise HTTPException(status_code=404, detail='NetBox Endpoint not found.')
+    session.delete(netbox_endpoint)
+    session.commit()
+    return {'message': 'NetBox Endpoint deleted.'}
+
+
 @router.get("/status")
 async def netbox_status(
     nb: NetboxSessionDep
