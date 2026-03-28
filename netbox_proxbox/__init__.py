76c77e8e5d4dc182d316ce22251caa9bd565f2c8
Emerson Felipe <emersonfelipe.2003@gmail.com>
2026-03-28 04:34:44 +0000

    update: widen NetBox plugin compatibility for 4.5

--- netbox_proxbox/__init__.py
+++ netbox_proxbox/__init__.py
@@ -1,6 +1,7 @@
 # Netbox plugin related import
 from netbox.plugins import PluginConfig
 
+
 class ProxboxConfig(PluginConfig):
     name = "netbox_proxbox"
     verbose_name = "Proxbox"
@@ -8,11 +9,12 @@ class ProxboxConfig(PluginConfig):
     version = "0.0.6b2"
     author = "Emerson Felipe (@emersonfelipesp)"
     author_email = "emersonfelipe.2003@gmail.com"
-    min_version = '4.2.0'
-    max_version = '4.3.9'
+    min_version = "4.2.0"
+    max_version = "4.5.99"
     base_url = "proxbox"
     required_settings = []
 
+
 config = ProxboxConfig
 
-#from . import proxbox_api
+# from . import proxbox_api
