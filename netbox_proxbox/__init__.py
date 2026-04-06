3711ff7035cca1a9bad0fbee7be90a0fb837863d
Emerson Felipe <emersonfelipe.2003@gmail.com>
2026-04-06 19:05:37 +0000

    chore: bump version to 0.0.11

--- netbox_proxbox/__init__.py
+++ netbox_proxbox/__init__.py
@@ -31,7 +31,7 @@ class ProxboxConfig(PluginConfig):
     name = "netbox_proxbox"
     verbose_name = "Proxbox"
     description = "Integrates Proxmox and Netbox"
-    version = "0.0.10"
+    version = "0.0.11"
     author = "Emerson Felipe (@emersonfelipesp)"
     author_email = "emersonfelipe.2003@gmail.com"
     min_version = "4.5.0"
