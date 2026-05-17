fa8b047d65dddaf6fbe2d84e21e72c1104b25f49
Emerson Felipe <emersonfelipe.2003@gmail.com>
2026-05-17 20:14:27 -0300

    chore(release): bump to 0.0.16rc2
    rc2 retag picks up the proxbox-api 0.0.12rc3 TestPyPI version (set via
    PROXBOX_API_TESTPYPI_VERSION repo variable) for the e2e-docker stage.

--- netbox_proxbox/__init__.py
+++ netbox_proxbox/__init__.py
@@ -146,7 +146,7 @@ class ProxboxConfig(PluginConfig):
     name = "netbox_proxbox"
     verbose_name = "Proxbox"
     description = "Integrates Proxmox and Netbox"
-    version = "0.0.16rc1"
+    version = "0.0.16rc2"
     author = "Emerson Felipe (@emersonfelipesp)"
     author_email = "emersonfelipe.2003@gmail.com"
     min_version = "4.5.8"
