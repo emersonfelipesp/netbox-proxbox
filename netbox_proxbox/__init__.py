334f6cbfaaa5e652d155e429e1143f00151bc58c
Emerson Felipe <emerson@netdevopsbr.com>
2026-06-01 17:10:46 +0000

    ci: fix post release metadata

--- netbox_proxbox/__init__.py
+++ netbox_proxbox/__init__.py
@@ -146,7 +146,7 @@ class ProxboxConfig(PluginConfig):
     name = "netbox_proxbox"
     verbose_name = "Proxbox"
     description = "Integrates Proxmox and Netbox"
-    version = "0.0.19"
+    version = "0.0.19.post1"
     author = "Emerson Felipe (@emersonfelipesp)"
     author_email = "emersonfelipe.2003@gmail.com"
     min_version = "4.5.8"
